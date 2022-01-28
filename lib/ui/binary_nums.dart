8513378b04f41e58109553cad43036f27a90dc11
hirotimes <hrtk.y061@gmail.com>
2022-01-29 08:45:46 +0900

    feat: ファイル作成

---
+++ lib/ui/binary_nums.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class BinaryNums extends StatelessWidget {
+  // const BinaryNums({Key? key}) : super(key: key);
+  const BinaryNums({Key? key}): super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
